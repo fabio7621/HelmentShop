d280475a701af9d446383235347cc54187b354ea
fabio7621 <d6200121a@gmail.com>
2024-03-06 00:52:43 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+	history: createWebHashHistory(),
+	linkActiveClass: "active",
+	routes: [
+		{
+			path: "/",
+			component: () => import("../views/FrontView.vue"),
+			children: [
+				{
+					path: "/",
+					name: "home",
+					component: () => import("../views/FrontView/HomeView.vue"),
+				},
+				{
+					path: "/about",
+					name: "about",
+					component: () => import("../views/FrontView/AboutView.vue"),
+				},
+				{
+					path: "/product",
+					name: "product",
+					component: () => import("../views/FrontView/ProductView.vue"),
+				},
+				{
+					path: "/products",
+					name: "products",
+					component: () => import("../views/FrontView/ProductsView.vue"),
+				},
+				{
+					path: "/checkout",
+					name: "checkout",
+					component: () => import("../views/FrontView/"),
+				},
+			],
+		},
+	],
+});
+
+export default router;
