9c24d238ee209fb9d9ba38ea4f67b566293ef0b4
fabio7621 <d6200121a@gmail.com>
2024-03-17 11:08:27 +0800

    ver1.0.5

--- src/main.js
+++ src/main.js
@@ -30,3 +30,29 @@ app.config.globalProperties.$filters = {
 	date,
 	currency,
 };
+app.component("VueLoading", Loading);
+
+import { Form, Field, ErrorMessage, defineRule, configure } from "vee-validate";
+import * as AllRules from "@vee-validate/rules";
+import { localize, setLocale } from "@vee-validate/i18n";
+import zhTW from "@vee-validate/i18n/dist/locale/zh_TW.json";
+/**
+ * 這裡是將所有 vee-validate 的規則載入
+ */
+Object.keys(AllRules).forEach((rule) => {
+	defineRule(rule, AllRules[rule]);
+});
+
+/**
+ * 這裡是設定 vee-validate 的語系
+ */
+configure({
+	generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+	validateOnInput: true, // 當輸入任何內容直接進行驗證
+});
+// 設定預設語系
+setLocale("zh_TW");
+app.component("VueLoading", Loading);
+app.component("VeeForm", Form);
+app.component("VeeField", Field);
+app.component("ErrorMessage", ErrorMessage);
--- src/views/FrontView/CheckoutView.vue
+++ src/views/FrontView/CheckoutView.vue
@@ -95,7 +95,8 @@
 					</div>
 					<div class="section-checkout-order">
 						<div class="checkout-order-main">
-							<form
+							<VeeForm
+								v-slot="{ errors }"
 								@submit="createOrder"
 								ref="form"
 								class="checkout-order-box"
@@ -105,47 +106,63 @@
 									<label class="mb-md-3 mb-3" for="email"
 										><span>＊</span>email
 									</label>
-									<input
+									<VeeField
 										type="email"
+										name="email"
 										class="w-100 mb-md-5 mb-3 form-control"
 										id="email"
 										placeholder="email"
+										rules="email|required"
+										:class="{ 'is-invalid': errors['email'] }"
 										v-model="form.user.email"
 									/>
+									<ErrorMessage name="email" class="invalid-feedback" />
 								</div>
 								<div class="section-order-item">
 									<label class="mb-md-3 mb-3" for="yourname">
 										<span>＊</span>收件人姓名
 									</label>
-									<input
+									<VeeField
+										:class="{ 'is-invalid': errors['姓名'] }"
 										placeholder="收件人姓名"
+										rules="required"
 										class="w-100 mb-md-5 mb-3 form-control"
 										type="text"
 										id="yourname"
+										name="姓名"
 										v-model="form.user.name"
 									/>
+									<ErrorMessage name="姓名" class="invalid-feedback" />
 								</div>
 								<div class="section-order-item">
 									<label class="mb-md-3 mb-3" for="">
 										<span>＊</span>收件人電話
 									</label>
-									<input
+									<VeeField
+										:class="{ 'is-invalid': errors['電話'] }"
 										placeholder="收件人電話"
+										rules="required|min:8|max:10"
 										class="w-100 mb-md-5 mb-3 form-control"
 										type="tel"
 										v-model="form.user.tel"
+										name="電話"
 									/>
+									<ErrorMessage name="電話" class="invalid-feedback" />
 								</div>
 								<div class="section-order-item">
 									<label class="mb-md-3 mb-3" for="">
 										<span>＊</span>收件人地址
 									</label>
-									<input
-										placeholder="收件人地址"
+									<VeeField
+										:class="{ 'is-invalid': errors['地址'] }"
+										placeholder="請輸入地址"
+										rules="required"
 										class="w-100 mb-md-5 mb-3 form-control"
 										type="text"
 										v-model="form.user.address"
+										name="地址"
 									/>
+									<ErrorMessage name="地址" class="invalid-feedback" />
 								</div>
 								<div class="section-order-item">
 									<label class="mb-md-3 mb-3" for=""><span>＊</span>留言</label>
@@ -157,7 +174,7 @@
 										v-model="form.message"
 									></textarea>
 								</div>
-							</form>
+							</VeeForm>
 
 							<button
 								@click="createOrder()"
