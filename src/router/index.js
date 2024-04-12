d09857ccc868f09e8d53d7142494029b8a83c915
fabio7621 <d6200121a@gmail.com>
2024-04-12 19:59:11 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -62,7 +62,7 @@ const router = createRouter({
     {
       path: "/login",
       name: "login",
-      component: () => import("../views/LoginView.vue"),
+      component: () => import("../views/LoginhereView.vue"),
     },
     {
       path: "/dashbord",
